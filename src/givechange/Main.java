ba22a941289ce9381a11d6cb6c7ed7ad65b6507f
“Dzmitry <d.slizhou@gmail.com>
2022-12-11 23:33:52 -0800

    All tests passed giveChange

--- src/givechange/Main.java
+++ src/givechange/Main.java
@@ -2,49 +2,51 @@ package givechange;
 
 class Main {
 
-    // write your solution in this function
     public static double GiveChange(CatalogItem[] catalogItems, OrderedItem[] orderedItems, double moneyProvided)
             throws IllegalArgumentException, ItemNotInCatalogException, InsufficientFundsException {
-        double changeExpected = 0.0;
-        double totalOrdered = 0.0;
-        try {
-            if (moneyProvided < 0) {
-                throw new IllegalArgumentException();
-            }
-            for (int i = 0; i < orderedItems.length; i++) {
-                int orderedItemId = orderedItems[i].getId();
-                int orderedItemQuantity = orderedItems[i].getQuantity();
 
-                for (int b = 0; i < catalogItems.length; i++) {
-                    int catalogItemId = catalogItems[b].getId();
-                    double catalogItemPrice = catalogItems[b].getPrice();
-                    if (orderedItemId == catalogItemId) {
-                        // double price = catalogItems[b].getPrice();
-                        double purchasedItem = catalogItemPrice * orderedItemQuantity;
-                        totalOrdered = totalOrdered + purchasedItem;
-                    }
-                }
-            }
-            if (moneyProvided < 0){
-                throw new InsufficientFundsException();
-            }
-            String s = "null";
-            if (totalOrdered == 0.0){
-                throw new ItemNotInCatalogException();
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.print(e.getMessage());
-        } catch (ItemNotInCatalogException e){
-            System.err.println(e.getMessage());
-        } catch (InsufficientFundsException e){
-            System.err.println(e.getMessage());
+        if ((catalogItems == null) && (orderedItems == null)){
+            return moneyProvided;
+        }
+        if (catalogItems == null){
+            throw new ItemNotInCatalogException();
+        }
+        if (orderedItems == null){
+            return moneyProvided;
+        }
+        if (moneyProvided < 0){
+            throw new IllegalArgumentException();
         }
 
-        changeExpected = moneyProvided - totalOrdered;
-        return changeExpected;
+        double toReturn = 0.0;
+        double orderedByCustomer = 0.0;
+
+        for (int i = 0; i < orderedItems.length; i++) {
+            if (orderedItems[i] == null){
+                continue;
+            }
+            int orderedItemId = orderedItems[i].getId();
+            if (orderedItemId > catalogItems.length){
+                throw new ItemNotInCatalogException();
+            }
+            int orderedItemQuantity = orderedItems[i].getQuantity();
+            for (int b = 0; b < catalogItems.length; b++) {
+               if (catalogItems[b].getId() == orderedItemId) {
+                    double catalogItemPrice = catalogItems[b].getPrice();
+                    double moneySpendForItem = catalogItemPrice * orderedItemQuantity;
+                    orderedByCustomer = orderedByCustomer + moneySpendForItem;
+                    break;
+                }
+            }
+        }
+        if (moneyProvided <  orderedByCustomer){
+            throw new InsufficientFundsException();
+        }
+
+        toReturn = moneyProvided - orderedByCustomer;
+        return toReturn;
     }
 
-    // do not change the following code
     public static void main(String[] args) {
         TestRunner tr = new TestRunner();
         tr.main();
